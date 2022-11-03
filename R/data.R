5687e4b63c4703b0c725877e52aba265b4ceb55d
parmsam <parmartsam@gmail.com>
2022-11-03 10:06:31 -0400

    document data to finish

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' @title Pirate Speak Lookup Table
+#' @description This lookup table maps English to pirate speak.
+#' @format A data frame with 182 rows and 2 variables:
+#' \describe{
+#'   \item{\code{orig}}{character english word}
+#'   \item{\code{new}}{character pirate word}
+#'}
+#' @source \url{https://github.com/ntoll/arrr/blob/master/arrr.py}
+#' @source \url{https://github.com/mikewesthad/pirate-speak/blob/master/lib/pirate-speak.js}
+"pirate_lookup"
+
+#' @title Pirate Phrases
+#' @description Collection of phrases that a pirate might randomly say.
+#' @format A data frame with 17 rows and 1 variables:
+#' \describe{
+#'   \item{\code{phrase}}{character phrase in pirate speak}
+#'}
+#' @source \url{https://github.com/ntoll/arrr/blob/master/arrr.py}
+"pirate_phrases"
